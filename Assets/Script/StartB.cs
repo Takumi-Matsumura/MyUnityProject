0d74c0453c19aea1cb154ff52236cfedcdb03519
Takumi <taktak55@ac.cyberhome.ne.jp>
2021-08-31 10:38:13 +0900

    シーン遷移<フェードイン・アウト>

---
+++ Assets/Script/StartB.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartB : MonoBehaviour
+{
+    public void Onclick()
+    {
+        //ここで移りたいシーンを指定します。
+        FadeManager.Instance.LoadScene("Tutorial",1.0f);
+    }
+}
