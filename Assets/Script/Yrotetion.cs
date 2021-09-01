eb81f0dd19bbdb2f004ed9f4473644c751369f11
Takumi <taktak55@ac.cyberhome.ne.jp>
2021-09-01 19:12:02 +0900

    Day3

--- Assets/Script/Yrotetion.cs
+++ Assets/Script/Yrotetion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotetion : MonoBehaviour
+public class Yrotetion : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class rotetion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0.2f, 0));
+        transform.Rotate(new Vector3(0, 0, 0.1f));
     }
 }
